86bb1e54679199918e598c8d56675cdb093c40d3
Marcos Jaciel Damian Nava <marcosnava73@gmail.com>
2026-03-02 19:15:24 -0600

    Initial Commit

---
+++ InventoryTests/InventoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  InventoryTests.swift
+//  InventoryTests
+//
+//  Created by Damian Nava on 02/03/26.
+//
+
+import Testing
+@testable import Inventory
+
+struct InventoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
